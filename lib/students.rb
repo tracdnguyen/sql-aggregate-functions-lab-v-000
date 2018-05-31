7b630f30c06adbec973ef3378568c144e6b26eb8
Trac Nguyen <nguyendtrac@gmail.com>
2018-05-31 19:26:39 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,17 +5,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  "SELECT MIN(gpa) FROM cats;"
+  "SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM cats;"
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) FROM cats;"
+  "SELECT SUM(tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade
-  "SELECT AVG(gpa) FROM cats WHERE grade < 10;"
+  "SELECT AVG(gpa) FROM students WHERE grade < 10;"
 end
